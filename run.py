37eb53fc5f315b810a6adea35a52985a5b874da1
wintor12 <wintor12@gmail.com>
2015-08-05 15:22:10 -0400

    add setting file

--- run.py
+++ run.py
@@ -13,19 +13,22 @@ def checkPos(word):
 	else:
 		return False
 
+def readSetting():
+	path = os.getcwd()
+	with open(join(path, 'setting'), 'r') as f:
+		setting = f.readlines()
+	return setting
 
-# s = 'As MH370 investigators prepare to analyze a Boeing 777 wing component at a French laboratory, ' \
-#  'the coastal town where it was found is looking for ways to cement its place in aviation history. ' \
-#  'If experts confirm the flaperon is from the missing Malaysia Airlines plane, ' \
-#  'it will be the first piece of wreckage found since the jet disappeared 16 months ago. ' \
-#  'That would mean the farming town of Saint Andre played a critical role in solving one of the greatest aviation mysteries of modern times. '
+def readDoc():
+	path = os.getcwd()
+	p = open(join(path, 'doc'), 'r')
+	s = p.read()
+	p.close()
+	return s
 
-s = 'According to Carnell, Behn\'s choice of literary form underscores the differences between her own approach' \
-' to crafting a tragic story and that taken in the dramatic tragedies, with their artificial distinction between the public and private spheres. ' \
-'Behn\'s novels engage in the political dialogue of her era by demonstrating that the good of the nation ' \
-'ultimately encompasses more than the good of the public figures who rule it.'
-
-model_path = '/Users/tongwang/Desktop/exp/lda/GoogleNews-vectors-negative300.bin'
+s = readDoc()
+setting = readSetting()
+model_path = setting[0]
 model = gensim.models.Word2Vec.load_word2vec_format(model_path, binary=True)
 
 res = ''
